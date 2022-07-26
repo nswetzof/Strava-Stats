fdf2d57f241a7bcfbfc82ade0154bcd3f12f1179
Nathan Swetzof <nswetzof@gmail.com>
2022-07-25 18:52:54 -0700

    Create initial html and javascript files.

---
+++ script.js
@@ -0,0 +1,12 @@
+const client_id = '53575'
+const client_secret = '0c7deae6ba652c65931094be526fc843b15f19fd';
+const access_token = '4b454f427b408868d378c86214e159ef496071d0';
+const refresh_token = 'e3327102378d5d72c279112aa4262ac0807220f4';
+
+let url = 
+
+// const mainElement = document.querySelector('main');
+// const para = document.createElement('p');
+// para.style.color = 'orange';
+// para.textContent = 'test';
+// mainElement.appendChild(para);
