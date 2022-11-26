fb5f34a2553ed11bd7a9ad8e1f63ee379677949e
Nathan Swetzof <nswetzof@gmail.com>
2022-11-26 13:15:40 -0800

    Add implementation to get access token, refresh token, and basic athlete information.

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 const client_id = '53575'
 const client_secret = '647c3ad896a07628c0337e0b92a5fd3b20adbfb7';
-const access_token = '4b454f427b408868d378c86214e159ef496071d0';
-const refresh_token = 'e3327102378d5d72c279112aa4262ac0807220f4';
+// const access_token = '4b454f427b408868d378c86214e159ef496071d0';
+// const refresh_token = 'e3327102378d5d72c279112aa4262ac0807220f4';
 
 const submitBtn = document.querySelector(".submit");
 
-window.addEventListener("load", authorize);
-submitBtn.addEventListener("click", getCredentials)
+// window.addEventListener("load", authorize);
+submitBtn.addEventListener("click", e => getCredentials(e))
 
+const main = document.querySelector("main");
+
+const para = document.createElement("p");
+para.textContent = "Test";
+main.appendChild(para);
+
+run();
+
+async function run() {
+
+  const message = await authorize();
+  // message.then((result) => console.log(result));
+  console.log(message);
+
+  let access_token = message.access_token;
+  let refresh_token = message.refresh_token;
+  const athlete = message.athlete;
+
+  console.log([access_token, refresh_token]);
+}
 
 // var StravaApiV3 = require('strava_api_v3');
 // var defaultClient = StravaApiV3.ApiClient.instance;
@@ -29,19 +49,17 @@ submitBtn.addEventListener("click", getCredentials)
 
 function getCredentials(e) {
   e.preventDefault();
-
-  console.log("redirecting...");
   window.location.href = `http://www.strava.com/oauth/authorize?client_id=${client_id}&response_type=code&approval_propt=force&scope=read_all&redirect_uri=http://localhost:8000`;
 }
 
-function authorize(e) {
+async function authorize() {
   let addr = new URL(window.location.href);
 
   if(addr.searchParams.has("code")) {
 
     let code = addr.searchParams.get("code");
 
-    const message = fetch("https://www.strava.com/api/v3/oauth/token", {
+    const messagePromise = await fetch("https://www.strava.com/api/v3/oauth/token", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
@@ -53,13 +71,30 @@ function authorize(e) {
         "code": code,
         "grant_type": "authorization_code"
       } )
-    }).then((response) => response.json());
-    console.log(message);
+    }).then((response) => {
+        if(!response.ok)
+          throw new Error(`HTTP error: ${response.status}`);
+        
+        return response.json();
+    }).then((data) => {
+        return data
+    }).catch((error) => {
+        console.error(`Authentication error: ${error}`);
+    });
 
-
-    // window.location.href = `https://www.strava.com/oauth/authorize \
-    //                         -d client_id=${client_id} \
-    //                         -d client_secret=${client_secret} \
-    //                         -d code=${};
+    return messagePromise;
   }
+}
+
+async function getTokens(response) {
+  let result = await response.then((data) => {
+    return data;
+  });
+  console.log("here is the result:");
+  console.log(result);
+
+  let access = result.access_token;
+  let refresh = result.refresh_token;
+
+  return [access, refresh];
 }
