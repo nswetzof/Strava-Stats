860515a5c3f55920f8a8ba440ce950c6da3a62ce
Nathan Swetzof <nswetzof@gmail.com>
2023-03-04 15:30:19 -0800

    Bug fixes and additional credential checking.

--- index.js
+++ index.js
@@ -14,22 +14,23 @@ const client_secret = '0f3f0cb4cfe3232afdc5e5e5aba3d081c4beceb0';
 let access_token = null;
 let refresh_token = null;
 let athlete = null;
-let scope = "read";
-
-let db; // hold an instance of a db object to store the IndexedDB login data and athlete statistics
-openDB();
-
+let scope = "readonly";
 
 const submitBtn = document.querySelector(".main_button");
+submitBtn.addEventListener("click", e => redirect(e, "read_all,activity:read_all"));
+
 const searchTerm = document.querySelector(".search");
 const searchForm = document.querySelector(".search_form");
 const redirectBtn = document.querySelector(".redirect");  // TODO: maybe doesn't go in this file
 
 // window.addEventListener("load", authorize);
-submitBtn.addEventListener("click", e => redirect(e, "read_all,activity:read_all"));
 searchForm.addEventListener("submit", e => {
   e.preventDefault();
-})
+});
+
+let db; // hold an instance of a db object to store the IndexedDB login data and athlete statistics
+openDB();
+
 // redirectBtn.addEventListener("click", e => redirect(e));  // TODO: FIGURE THIS PART OUT.  CALLS FUNCTION WHEN LINK PRESSED FROM AUTH ERROR PAGE
 
 const main = document.querySelector("main");
@@ -38,29 +39,37 @@ const para = document.createElement("p");
 para.textContent = "Test";
 main.appendChild(para);
 
-console.log("finished");
+// async function run(callback) {
+
+// }
 
 async function run() {
+  console.log("running main loop...");
+  // const athlete = localStorage.getItem("user");
+  // checkStoredCredentials(athlete, scope);
 
-  // if(localStorage.getItem("user")) {
+  if(localStorage.getItem("user")) {
+    const athlete = localStorage.getItem("user");
+    checkStoredCredentials(athlete, scope);
+  }
+  else {
+    await authorize().then((response) => {
+      console.log(response);
+      access_token = response.access_token;
+      refresh_token = response.refresh_token;
+      athlete = response.athlete.id;
 
-  // }
-  await authorize().then((response) => {
-    console.log(response);
-    access_token = response.access_token;
-    refresh_token = response.refresh_token;
-    athlete = response.athlete.id;
-
-    const expiration = response.expires_at;
-    const name = `${response.athlete.firstname} ${response.athlete.lastname}`;
-    
-    localStorage.setItem("user", athlete);
-    
-    addCredentials(db, athlete, scope, access_token, expiration, refresh_token);
-    checkAuthentication(athlete, "activity:read_all");
-  }).catch(error => {
-    console.error(`Authorization Error: ${error}`);
-  });
+      const expiration = response.expires_at;
+      const name = `${response.athlete.firstname} ${response.athlete.lastname}`;
+      
+      localStorage.setItem("user", athlete);
+      
+      addCredentials(db, athlete, scope, access_token, expiration, refresh_token);
+      // checkAuthentication(athlete, "activity:read_all");
+    }).catch(error => {
+      console.error(`Authorization Error: ${error}`);
+    });
+  }
 
   // await refresh(refresh_token).then(result => {
   //   access_token = result.access_token;
@@ -77,6 +86,17 @@ async function run() {
 
   // let test = await getLoggedInAthleteActivities(access_token); 
 }
+
+// function addListeners() {
+//   const searchTerm = document.querySelector(".search");
+//   const searchForm = document.querySelector(".search_form");
+//   const redirectBtn = document.querySelector(".redirect");  // TODO: maybe doesn't go in this file
+
+//   // window.addEventListener("load", authorize);
+//   searchForm.addEventListener("submit", e => {
+//     e.preventDefault();
+//   });
+// }
 
 /* Authorization functions (getTokens is unused) */
 
@@ -119,6 +139,8 @@ async function authorize() {
 
     return messagePromise;
   }
+
+  else return null;
 }
 
 async function refresh(token) {
@@ -274,7 +296,6 @@ function openDB() {
     const accessStore = db.createObjectStore("access", {keyPath: "id"});
     accessStore.createIndex("access_token", "access_token", {unique: false});
     accessStore.createIndex("expiration", "expiration", {unique: false});
-    refreshStore.createIndex("scope", "scope", {unique : false});
 
     // Create a refresh token object store
     const refreshStore = db.createObjectStore("refresh", {keyPath: "id"});
@@ -332,15 +353,20 @@ function addCredentials(db, id, scope, access_token, expiration, refresh_token) 
   };
 }
 
-async function checkAuthentication(id, scope) {
-  if(localStorage.getItem("user")) {
+async function checkStoredCredentials(id, scope) {
+  if(localStorage.getItem("user")) {  // TODO: probably is covered outside of this function and can be removed
     const transaction = db.transaction(["access", "refresh"], "readwrite");
     const accessStore = transaction.objectStore("access");
     const accessRequest = accessStore.get(id);
 
-    accessRequest.onsuccess = (acc_event) => {  // TODO: need to add callback to Strava API function we're calling?
-      const access_data = acc_event.target.result;
-      if(!valid_scope(access_data.scope, scope)) {  // if scope not sufficient, reauthorize to obtain the desired scope
+    accessRequest.onsuccess = (acc_event) => {
+      const access_data = accessRequest.result;
+      console.log(access_data);
+
+      if(!access_data) {
+          alert("Please log in to access the database.");
+      }
+      else if(!valid_scope(access_data.scope, scope)) {  // if scope not sufficient, reauthorize to obtain the desired scope
         alert(`Insufficient authorization.  Provide authorization for ${scope} scope`);
         redirect(acc_event, scope);
       }
@@ -376,34 +402,34 @@ async function checkAuthentication(id, scope) {
         required_scope: The required scope necessary for a call to the Strava API to be approved
   */
 
-  function valid_scope(user_scope, required_scope) {
-    scopes = user_scope.split(",");
-    
-    if(scopes.includes(required_scope))
-      return true;
-
-    return false;
-  }
-
-  const transaction = db.transaction(["access", "refresh"], "readwrite");
-
-  transaction.oncomplete = (event) => {
-    console.log("Transaction completed");  // TODO: update message
-  }
-  transaction.onerror = (event) => {
-    console.error(`Error in transaction: ${event.target.result.errorCode}`);
-  }
-
-  const accessStore = transaction.objectStore("access");
-
-  const request = accessStore.get(id);
-
-  request.onsuccess = (event) => {
-    let access =  event.target.result;
-    console.log(access);
-  }
 }
 
+function valid_scope(user_scope, required_scope) {
+  scopes = user_scope.split(",");
+  
+  if(scopes.includes(required_scope))
+    return true;
+
+  return false;
+}
+
+// const transaction = db.transaction(["access", "refresh"], "readwrite");
+
+// transaction.oncomplete = (event) => {
+//   console.log("Transaction completed");  // TODO: update message
+// }
+// transaction.onerror = (event) => {
+//   console.error(`Error in transaction: ${event.target.result.errorCode}`);
+// }
+
+// const accessStore = transaction.objectStore("access");
+
+// const request = accessStore.get(id);
+
+// request.onsuccess = (event) => {
+//   let access =  event.target.result;
+//   console.log(access);
+// }
 // var StravaApiV3 = require('strava_api_v3');
 // var defaultClient = StravaApiV3.ApiClient.instance;
 
