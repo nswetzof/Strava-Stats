bcf478487ca656b7b5703a20e1d7fc0ed71204d6
Nathan Swetzof <nswetzof@gmail.com>
2023-04-12 19:57:41 -0700

    Fix bugs in DBase.js.

--- modules/DBase.js
+++ modules/DBase.js
@@ -1,14 +1,102 @@
-export * from "Dbase.js";
+// export * from "Dbase.js";
 const DB_NAME = "AccessTokens";
 const DB_VERSION = 1;
 
 let db; // hold an instance of a db object to store the IndexedDB login data and athlete statistics
-openDB();
+// openDB();
 
 function useDatabase() {
   db.onversionchange = (event) => {
     db.close();
     alert("A new version of this page is ready.  Please reload or close this tab.");
+  };
+}
+
+function openDB(callback) {
+  const request = window.indexedDB.open("AthleteInformation", 1);
+
+  request.onblocked = (event) => {
+    alert("Please close all other tabs with this site open.");
+  };
+
+  request.onerror = (event) => {
+    console.error(`Error: ${event.target.errorCode}`);
+  };
+
+  request.onsuccess = (event) => {
+    console.log("Successfully opened the database.");
+
+    db = event.target.result;
+
+    callback(); // execute callback function
+  };
+  
+  request.onupgradeneeded = (event) => {
+    // Save the IDBDatabase interface
+    db = event.target.result;
+    
+    db.onerror = (event) => {
+      console.error(`Database error: ${event.target.errorCode}`);
+    }
+
+    // Create an access token object store
+    const accessStore = db.createObjectStore("access", {keyPath: "id"});
+    accessStore.createIndex("access_token", "access_token", {unique: false});
+    accessStore.createIndex("expiration", "expiration", {unique: false});
+
+    // Create a refresh token object store
+    const refreshStore = db.createObjectStore("refresh", {keyPath: "id"});
+    refreshStore.createIndex("refresh_token", "refresh_token", {unique: false});
+    refreshStore.createIndex("scope", "scope", {unique : false});
+
+    // Create athlete activities object store
+    const activityStore = db.createObjectStore("activities", {keyPath : "id"})
+    activityStore.createIndex("external_id", "external_id", {unique : false});
+    activityStore.createIndex("upload_id", "upload_id", {unique : false});
+    activityStore.createIndex("athlete", "athlete", {unique : true});
+    activityStore.createIndex("name", "name", {unique : false});
+    activityStore.createIndex("distance", "distance", {unique : false});
+    activityStore.createIndex("moving_time", "moving_time", {unique : false});
+    activityStore.createIndex("elapsed_time", "elapsed_time", {unique : false});
+    activityStore.createIndex("total_elevation_gain", "total_elevation_gain", {unique : false});
+    activityStore.createIndex("elev_high", "elev_high", {unique : false});
+    activityStore.createIndex("elev_low", "elev_low", {unique : false});
+    activityStore.createIndex("sport_type", "sport_type", {unique : false});
+    activityStore.createIndex("start_date", "start_date", {unique : false});
+    activityStore.createIndex("start_date_local", "start_date_local", {unique : false});
+    activityStore.createIndex("timezone", "timezone", {unique : false});
+    activityStore.createIndex("start_latlng", "start_latlng", {unique : false});
+    activityStore.createIndex("end_latlng", "end_latlng", {unique : false});
+    activityStore.createIndex("achievement_count", "achievement_count", {unique : false});
+    activityStore.createIndex("kudos_count", "kudos_count", {unique : false});
+    activityStore.createIndex("comment_count", "comment_count", {unique : false});
+    activityStore.createIndex("athlete_count_count", "athlete_count", {unique : false});
+    activityStore.createIndex("photo_count", "photo_count", {unique : false});
+    activityStore.createIndex("total_photo_count", "total_photo_count", {unique : false});
+    activityStore.createIndex("map", "map", {unique : false});
+    activityStore.createIndex("trainer", "trainer", {unique : false});
+    activityStore.createIndex("commute", "commute", {unique : false});
+    activityStore.createIndex("manual", "manual", {unique : false});
+    activityStore.createIndex("private", "private", {unique : false});
+    activityStore.createIndex("flagged", "flagged", {unique : false});
+    activityStore.createIndex("workout_type", "workout_type", {unique : false});
+    activityStore.createIndex("upload_id_str", "upload_id_str", {unique : false});
+    activityStore.createIndex("average_speed", "average_speed", {unique : false});
+    activityStore.createIndex("max_speed", "max_speed", {unique : false});
+    activityStore.createIndex("has_kudoed", "has_kudoed", {unique : false});
+    activityStore.createIndex("hide_from_home", "hide_from_home", {unique : false});
+    activityStore.createIndex("gear_id", "gear_id", {unique : false});
+    activityStore.createIndex("kilojoules", "kilojoules", {unique : false});
+    activityStore.createIndex("average_watts", "average_watts", {unique : false});
+    activityStore.createIndex("device_watts", "device_watts", {unique : false});
+    activityStore.createIndex("max_watts", "max_watts", {unique : false});
+    activityStore.createIndex("weighted_average_watts", "weighted_average_watts", {unique : false});
+
+
+    console.log( `Success! Created the following object stores:`);
+    for(let i = 0; i < db.objectStoreNames.length; i++) {
+      console.log(db.objectStoreNames[i]);
+    }
   };
 }
 
@@ -111,9 +199,7 @@ function checkStoredCredentials(id, scope, callback, ...args) {
       required_scope: The required scope necessary for a call to the Strava API to be approved
 */
 function valid_scope(user_scope, required_scope) {
-  scopes = user_scope.split(",");
-  
-  return scopes.includes(required_scope);
+  return user_scope.split(",").includes(required_scope);
 }
 
 function addActivityData(data) {
@@ -145,3 +231,6 @@ function addActivityData(data) {
 function removeActivityData(id) {
   throw new Error("Function has not been implemented");
 }
+
+export {db, useDatabase, openDB, addCredentials, checkStoredCredentials, addActivityData, removeActivityData};
+// export * from "DBase.js";
